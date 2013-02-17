7c6989c9ab3fb876ab550208fd1a41d64203ff0c
rexim <reximkut@gmail.com>
2013-02-17 13:16:30 +0700

    Added readme and copyright.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Gruber Darker
+
+Gruber Darker color theme for Emacs by Jason Blevins. A darker variant
+of the Gruber Dark theme for BBEdit by John Gruber. Adapted for
+deftheme by Alexey Kutepov a.k.a. rexim.
--- gruber-darker-theme.el
+++ gruber-darker-theme.el
@@ -1,7 +1,35 @@
+;;; gruber-darker-theme.el --- Gruber Darker color theme for Emacs.
+
+;; Copyright (C) 2009-2013 Jason R. Blevins
+
+;; Permission is hereby granted, free of charge, to any person
+;; obtaining a copy of this software and associated documentation
+;; files (the "Software"), to deal in the Software without
+;; restriction, including without limitation the rights to use, copy,
+;; modify, merge, publish, distribute, sublicense, and/or sell copies
+;; of the Software, and to permit persons to whom the Software is
+;; furnished to do so, subject to the following conditions:
+
+;; The above copyright notice and this permission notice shall be
+;; included in all copies or substantial portions of the Software.
+
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+;; EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+;; MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+;; NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+;; BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+;; ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+;; CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+;; SOFTWARE.
+
 (deftheme gruber-darker
   "Gruber Darker color theme for Emacs by Jason Blevins.
 A darker variant of the Gruber Dark theme for BBEdit by John
-Gruber.")
+Gruber. Adapted for deftheme by Alexey Kutepov a.k.a. rexim.")
+
+(custom-theme-set-variables
+ 'gruber-darker
+ '(frame-brackground-mode (quote dark)))
 
 (custom-theme-set-faces
  'gruber-darker
@@ -76,3 +104,5 @@ Gruber.")
 ;; Local Variables:
 ;; no-byte-compile: t
 ;; End:
+
+;;; gruber-darker-theme.el ends here.
