d9ec83f803493206fd89c8daa2ac8694b0192efb
rexim <reximkut@gmail.com>
2015-09-01 23:29:44 +0600

    Add some faces for ERC. Close #8

--- gruber-darker-theme.el
+++ gruber-darker-theme.el
@@ -146,6 +146,10 @@
    `(egg-text-base ((t (:foreground ,gruber-darker-fg))))
    `(egg-term ((t (:foreground ,gruber-darker-yellow))))
 
+   ;; ERC
+   `(erc-notice-face ((t (:foreground ,gruber-darker-wisteria))))
+   `(erc-timestamp-face ((t (:foreground ,gruber-darker-green))))
+
    ;; EShell
    `(eshell-ls-backup ((t (:foreground ,gruber-darker-quartz))))
    `(eshell-ls-directory ((t (:foreground ,gruber-darker-niagara))))
